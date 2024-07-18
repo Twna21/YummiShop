ee81643643636d721bc8bb74bd1edd49a67bdebb
twna21 <132243099+Twna21@users.noreply.github.com>
2024-07-18 17:34:40 +0700

    first commit

---
+++ BusinessObject/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+
+    public partial class Account
+    {
+        public int AccountID { get; set; }
+        public string Photo { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public byte Type { get; set; }
+        public byte Status { get; set; }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+Web use ASP.NET core, RazorPage, SignalR 
+DB: SQL Server
+
+
+How to RUN: 
+1: dotnet ef database update
+2: Set StartUP Project--> ShopWEB
+
+
